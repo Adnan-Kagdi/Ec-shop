58a6a190037c792c452ecc17805b13d1c4eeb793
Adnan Kagdi <kagdiadnan123@gmail.com>
2025-09-06 13:04:52 +0530

    basic changes

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,28 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 20000,
-    });
 
-    mongoose.connection.on('connected', () => {
-      console.log("MongoDB Connected");
-    });
+    mongoose.connection.on('connected',() => {
+        console.log("DB Connected");
+    })
 
-    mongoose.connection.on('error', (err) => {
-      console.error("MongoDB Connection Error:", err);
-    });
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
 
-    mongoose.connection.on('disconnected', () => {
-      console.warn("MongoDB Disconnected");
-    });
+}
 
-  } catch (err) {
-    console.error("MongoDB connection failed:", err);
-  }
-};
-
-export default connectDB;  
+export default connectDB;
